78f10a3183d666518199b5b298f4925a80b4be40
pablo de la cruz <pablodlc@gmail.com>
2021-04-09 14:29:16 -0700

    HTML 90% of 2/3 done for landing page and hidded game layout

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # code-quiz
+
+A DOM-manipulated, five question, timed quiz. The user has 75 seconds to answer five questions about coding. A right answer adds 10 seconds to the clock, a wrong answer subtracts 10 seconds. At the end, the user's score is the seconds remaining. The application uses local storage to save the top five high scores to a high score display.
+
+## User Story
+
+> AS A coding boot camp student  
+> I WANT to take a timed quiz on JavaScript fundamentals that stores high scores  
+> SO THAT I can gauge my progress compared to my peers
+
+## Acceptance Criteria
+
+> GIVEN I am taking a code quiz  
+> WHEN I click the start button  
+> THEN a timer starts and I am presented with a question  
+> WHEN I answer a question  
+> THEN I am presented with another question  
+> WHEN I answer a question incorrectly  
+> THEN time is subtracted from the clock  
+> WHEN all questions are answered or the timer reaches 0  
+> THEN the game is over  
+> WHEN the game is over  
+> THEN I can save my initials and score
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,26 +1,64 @@
-var timer = 0;
+var timer = 75;
+document.querySelector(".timer").textContent = timer;
 
 var body = document.body;
 var removeEl = document.querySelector("#removeMe");
+var hAndSEl = document.querySelector(".hideAndSeek");
 
 
-function startQuiz() {
-    // Removing existing elements and image
+function clearPage() {
+    // Removing existing elements
     removeEl.remove();
+    hAndSEl.style = "visibility: visible";
 }
 
-// For reference from first try
-// function createGameScreen() {
-//     // 
-    
-//     // Creating first question #homescreen HTML w/ style
-//     var columnEl = document.createElement("img");
-//     columnEl.setAttribute("src", "assets/images/column.jpg");
-//     document.querySelector(".flex-item").appendChild(columnEl);
+function right() {
+    timer = timer + 10;
+    document.querySelector(".timer").textContent = timer;
+}
 
-//     var questionEl = document.createElement("p");
-//     questionEl.textContent = "Commonly used data types DO NOT include:";
-//     document.querySelector(".flex-item").appendChild(questionEl);
+function wrong() {
+    timer = timer - 10;
+    if (timer <= 0) {
+        var hiScore = confirm("You lost and the browser broke my fourth wall.\nPress OK to see the High Scores");
+        if (hiScore) {
+            alert("This is the HiScore page. It was programatically updated by manipulating the DOM using JavaScript. It's retro-chic, kitcshy, and fun. It's like John Waters took Shigeru Miyamoto out for a night on the town, watching Jeopardy! on the bar's TVs and playing a sticky Trivial Pursuit board game, and a divine hand reached out and smacked Shigeru upside the head in that too dark Baltimore brewery.  He pulled out his laptop and there was born what you see here; a master's magnum opus HiScores page.")
+        }
+
+        location.reload();
+    }
+    document.querySelector(".timer").textContent = timer;
+}
 
 
-// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Adding event listeners
+// document.querySelector(btnA).addEventListener("click", function() {
+//     console.log("A");
+// });
+
+// document.querySelector(btnB).addEventListener("click", function() {
+//     console.log("B");
+// });
+
+// document.querySelector(btnC).addEventListener("click", function() {
+//     console.log("C");
+// });
+// document.querySelector(btnD).addEventListener("click", function() {
+//     console.log("D");
+// })
